01c68cd6592f49c9aa57c7e8424f16d087a21ed9
zfan <zfan@meitu.com>
2017-09-03 19:30:22 +0800

    【Feature】One LayerAnimation  about  circle  wave

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,9 @@
 This is My transition with two  ViewControllers.
+
+####Animation
+* Circle Wave
+* Circle DownLoad
+* DynamicAnimator 
+* POP 
+
+CI 、GPUImage 、粒子效果、
--- osbornTransition/osbornTransition/ViewController.m
+++ osbornTransition/osbornTransition/ViewController.m
@@ -14,10 +14,17 @@
 #import "LastViewController.h"
 #import "CustomerViewController.h"
 
+#define RGBA(r,g,b,a)     [UIColor colorWithRed:r / 255.f green:g / 255.f blue:b / 255.f alpha:a]
+#define RGBAHEX(hex,a)    RGBA((float)((hex & 0xFF0000) >> 16),(float)((hex & 0xFF00) >> 8),(float)(hex & 0xFF),a)
+
+#define  MainScreenSizeWidth    [[UIScreen mainScreen] bounds].size.width
+
 @interface ViewController ()<UINavigationControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIButton *lastVcBtn;
 @property (strong, nonatomic) BaseAnimationTransition *transitionAnimator;
+
+@property (nonatomic, strong) UIView *iconBackgroundView; //Test Animation
 
 @end
 
@@ -26,6 +33,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self addBackGroundView];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -74,14 +83,11 @@
     [self.navigationController pushViewController:customerController animated:YES];
 }
 
-
-
 //StoryBoard unwind  action
 - (IBAction)unwindToHome:(UIStoryboardSegue *)unwindSegue {
     //This is the Test for unwind
 
 }
-
 
 #pragma mark --UINavigationControllerDelegate
 - (id<UIViewControllerAnimatedTransitioning>)navigationController:(UINavigationController *)navigationController animationControllerForOperation:(UINavigationControllerOperation)operation fromViewController:(UIViewController *)fromVC toViewController:(UIViewController *)toVC {
@@ -100,7 +106,6 @@
 //    return self.interactionController;
 }
 
-
 #pragma mark --
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     
@@ -117,6 +122,49 @@
     }
 
 }
+
+
+
+#pragma mark --  waveAnimation Test
+
+- (void)addBackGroundView {
+    
+    _iconBackgroundView = [[UIView alloc] initWithFrame:CGRectMake(MainScreenSizeWidth/2.0-20, 4, 40, 40)];
+    _iconBackgroundView.backgroundColor = RGBAHEX(0xFF4970, .9f);
+    _iconBackgroundView.layer.cornerRadius = 20;
+    _iconBackgroundView.alpha = 0.f;
+    [self.view addSubview:_iconBackgroundView];
+    
+    [self waveAnimation];
+}
+
+- (void)waveAnimation {
+
+    CABasicAnimation *scale = [CABasicAnimation animation];
+    scale.keyPath   = @"transform.scale";
+    scale.fromValue = [NSNumber numberWithFloat:1.0];
+    scale.toValue   = [NSNumber numberWithFloat:1.5];
+    CABasicAnimation *scale1 = [CABasicAnimation animation];
+    scale1.keyPath = @"opacity";
+    scale1.fromValue =[NSNumber numberWithFloat:.5];
+    scale1.toValue =[NSNumber numberWithFloat:0.0];
+    CAAnimationGroup *group = [CAAnimationGroup animation];
+    group.animations = @[scale,scale1];
+    group.duration = 1.2;
+    group.repeatCount = 2;
+    group.fillMode = kCAFillModeForwards;
+    
+    CAAnimationGroup *allgroup = [CAAnimationGroup animation];
+    allgroup.duration  = 2.5;
+    allgroup.animations  = @[group];
+    allgroup.repeatCount = HUGE_VALF;
+    allgroup.removedOnCompletion = NO;
+    
+    [self.iconBackgroundView.layer addAnimation:allgroup forKey:nil];
+
+}
+
+
 
 
 @end
